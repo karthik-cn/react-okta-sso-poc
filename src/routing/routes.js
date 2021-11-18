c593b04578ea9bd1c26b15fbfff0baf0de465c0c
Karthik RP <karthik_puttaraju@condenast.com>
2021-11-18 20:55:25 +0530

    Removal of unwnated hook file, Simplified routes.js
    Signed-off-by: Karthik RP <karthik_puttaraju@condenast.com>

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -3,36 +3,40 @@ import Dashboard from '../components/Dashboard/Dashboard';
 import { SecureRoute, LoginCallback } from '@okta/okta-react';
 import { Route } from 'react-router-dom';
 
-const routeList = [
+const routeList = Object.freeze([
     {
         key: 'dashboard',
-        path: '/dashboard',
-        component: Dashboard,
-        secure: true
+        secure: true,
+        routeProps: {
+            path: '/dashboard',
+            component: Dashboard,
+        }
     },
     {
         key: 'loginCallBackLoader',
-        path: '/login/callback',
-        component: LoginCallback,
-        secure: false
+        secure: false,
+        routeProps: {
+            path: '/login/callback',
+            component: LoginCallback,
+        }
     },
     {
         key: 'home',
-        path: '/',
-        component: Home,
         secure: false,
-        exact: true,
+        routeProps: {
+            path: '/',
+            component: Home,
+            exact: true,    
+        }
     },
-]
+]);
 
 const getRoutes = () => (
     <>
         {
             routeList.map(route => {
-                if(route.secure) {
-                    return <SecureRoute path={route.path} component={route.component} exact={route.exact} key={route.key} />;
-                }
-                return <Route path={route.path} component={route.component} exact={route.exact} key={route.key} />
+                const RouteComponent = route.secure ? SecureRoute : Route;
+                return <RouteComponent key={route.key} {...route.routeProps} />;
             })
         }
     </>
